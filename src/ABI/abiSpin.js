5c7937cce06b1647ec086738aa3e82fc594511fb
semut <semut455coy@gmail.com>
2025-06-09 02:15:32 +0700

    Create abiSpin.js

---
+++ src/ABI/abiSpin.js
@@ -0,0 +1,13 @@
+const SPIN_ABI = [
+    {
+        "inputs": [],
+        "name": "startSpin",
+        "outputs": [],
+        "stateMutability": "payable",
+        "type": "function"
+      },
+    {
+        CA: '0xB8e9F72bd52575B61705e4376970E14C9F573D71',
+      }
+];
+module.exports = {  SPIN_ABI };
